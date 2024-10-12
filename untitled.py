bada937077ccc190b06ca964a6186f88ad20a921
FAVOUR OTABOR <favourotabor16@gmail.com>
2024-10-12 17:27:06 +0100

    Adjust Streamlit app for model parts

--- untitled.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pandas_profiling import ProfileReport
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-import pickle
