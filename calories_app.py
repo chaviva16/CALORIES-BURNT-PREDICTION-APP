495e84cfbafa7aca40de70e5b27bca69b73fff5a
FAVOUR OTABOR <favourotabor16@gmail.com>
2024-10-14 21:35:50 +0100

    Remove conflicted files

--- calories_app.py
+++ calories_app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eff79ba15a703bb77bbb75facc8b113533989459a62ba2c821135b2dcc2356cc
-size 2130
+oid sha256:a590dbc95efa81a4a555525a14fb262bdf71eb1319c86e18903a46f3a28a7200
+size 2221
