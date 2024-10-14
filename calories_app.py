425aa264474b435c826f1a32a2ae69239755dae1
FAVOUR OTABOR <favourotabor16@gmail.com>
2024-10-14 16:36:35 +0100

    Update path to download model file from GitHub

--- calories_app.py
+++ calories_app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d4741bb1f91ce574d4d5476e77aa3217334ccaa16a56b3426e074be8423b57b
-size 1530
+oid sha256:eff79ba15a703bb77bbb75facc8b113533989459a62ba2c821135b2dcc2356cc
+size 2130
