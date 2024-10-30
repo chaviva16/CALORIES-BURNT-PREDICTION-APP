1d67371e0e70b21575c4c4dcdbb44d67694427b3
FAVOUR OTABOR <favourotabor16@gmail.com>
2024-10-30 15:03:31 +0100

    updated calories app.py

--- calories_app.py
+++ calories_app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d344984edf6e7e932bee6c9c9775ab9ca72a2234ba1545f028ec2700cc315cc4
-size 5787
+oid sha256:10cc39e8a394cbb5aaf87a8cc5ae4c20e725d3ff953208c7881003b12343d224
+size 6136
