fcc7929cd7d5c871b9758e6bf7a25de2d142472f
FAVOUR OTABOR <favourotabor16@gmail.com>
2024-11-05 21:20:29 +0100

    Added calories_app.py and calories burnt prediction.ipynb

--- calories_app.py
+++ calories_app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cef485f150cd1bf8c050c9d1c6c8bf886e79f10edf904767243e2208ca6bd13
-size 6074
+oid sha256:6a4a2a25b8d3bf631c5f36b4ce71c2d7a73969fdf316785a1242e283602a464b
+size 6300
